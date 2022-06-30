055fc207c0fe42bfde33b655187e92fd2907d6ef
Alex Santos <alexdsantosv@gmail.com>
2022-06-30 17:00:10 -0600

    Agregar boton de conectar

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,31 @@ import { ethers } from "ethers";
 import Texto from "./Texto";
 
 function App() {
-  if ("ethereum" in window) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
+  function getAddress() {
+    if ("ethereum" in window) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    async function getWallet() {
-      const wallet = await signer.getAddress();
-      console.log(wallet);
+      async function getWallet() {
+        await provider.send("eth_requestAccounts", []);
+
+        const signer = provider.getSigner();
+        const wallet = await signer.getAddress();
+
+        const texto = document.getElementById("boton");
+        texto.innerText = wallet;
+      }
+
+      getWallet();
+    } else {
+      alert("Instala metamask");
     }
-
-    getWallet();
-  } else {
-    alert("Instala metamask");
   }
 
   return (
     <div className="App">
-      <Texto></Texto>
+      <button id="boton" className="btn" onClick={getAddress}>
+        CONNECT WALLET
+      </button>
     </div>
   );
 }
